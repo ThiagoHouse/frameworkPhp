0488db43a8f77b4a295a0cb01158e513c9409493
thyago-carlos <thyago-carlos@hotmail.com>
2021-04-22 12:16:27 -0300

    Cria o método index para invocar a view home

--- app/Controllers/Paginas.php
+++ app/Controllers/Paginas.php
@@ -1,9 +1,14 @@
 <?php
 
-class Paginas {
+class Paginas extends Controller{
+
+    public function index(){
+        $this->view('paginas/home');
+    }
+
     public function sobre()
     {
-        # Code
+        
     }
     
 }
