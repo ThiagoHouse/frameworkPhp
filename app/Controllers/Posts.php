2e4dbd5a5f462556f979c97023cd42536027abdd
thyago-carlos <thyago-carlos@hotmail.com>
2021-04-22 12:13:35 -0300

    Corrige o estilo do código

--- app/Controllers/Posts.php
+++ app/Controllers/Posts.php
@@ -1,5 +1,6 @@
 <?php
 
-class Posts {
+class Posts 
+{
     
 }
--- app/Libraries/Rota.php
+++ app/Libraries/Rota.php
@@ -1,7 +1,7 @@
 <?php
 
-class Rota {
-
+class Rota 
+{
     private $controlador = 'Paginas';
     private $metodo = 'index';
     private $parametros = [];
@@ -27,9 +27,8 @@ class Rota {
 
         $this->parametros = $url ?  array_values($url) : [];
         call_user_func_array([$this->controlador, $this->metodo], $this->parametros);
-
-        var_dump($this);     
     }
+
     private function url()
     {
         $url = filter_input(INPUT_GET,'url', FILTER_SANITIZE_URL);
