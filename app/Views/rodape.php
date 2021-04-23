605b98752745c2d671d822c5cd078b5530093818
thyago-carlos <thyago-carlos@hotmail.com>
2021-04-23 01:29:36 -0300

    Inclui topo e rodapé

---
+++ app/Views/rodape.php
@@ -0,0 +1,10 @@
+<footer class= "bg-dark p-5 text-light">
+    <div class="container">
+        <small>
+            Framwork Php Orientado a Ojetos e MVC. Versão: <?= APP_VERSAO ?>
+            <div class="border-top mt-3">
+                &COPY; 2021 - <?= date('Y') ?>    
+            </div>
+        </small>
+    </div>
+</footer>
--- public/index.php
+++ public/index.php
@@ -17,7 +17,9 @@ include './../app/Libraries/Controller.php';
 </head>
 <body>
     <?php
+        include '../app/Views/topo.php';
         $rotas = new Rota();
+        include '../app/Views/rodape.php';
     ?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" 
     integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous">
