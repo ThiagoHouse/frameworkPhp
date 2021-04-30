b34053547f49c09424f36548a2bb4032d9d82c05
thyago-carlos <thyago-carlos@hotmail.com>
2021-04-30 00:11:40 -0300

    Cria funções CRUD para manipular o banco de dados

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -8,6 +8,7 @@ class Database {
     private $banco = 'framework_php';
     private $porta = '3306';
     private $dbh;
+    private $stmt;
 
     public function __construct()
     {
@@ -22,5 +23,52 @@ class Database {
             print "Error!: " . $e->getMessage() . "<br/>";
             die();
         }
+    }
+
+    public function query($sql)
+    {
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    public function bind($parametro, $valor, $tipo = null)
+    {
+        if(is_null($tipo)):
+            switch (true):
+                case is_int($valor):
+                    $tipo = PDO::PARAM_INT;
+                break;
+                case is_bool($valor):
+                    $tipo = PDO::PARAM_BOOL;
+                break;
+                case is_null($valor):
+                    $tipo = PDO::PARAM_NULL;
+                break;
+                default:
+                $tipo = PDO::PARAM_STR;
+            endswitch;
+        endif;
+
+        $this->stmt->bindValue($parametro, $valor, $tipo);
+    }
+
+    public function executa()
+    {
+        return $this->stmt->execute();
+    }
+
+    public function resultado()
+    {
+        $this->executa();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function totalResultados()
+    {
+        return $this->stmt->rowCount();
+    }
+
+    public function ultimoIdInserido()
+    {
+        return $this->stmt->lastInsertId();
     }
 }
