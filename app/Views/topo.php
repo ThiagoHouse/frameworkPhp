0f925c2c745d88da3858dfb8893cddb7edb81fbc
thyago-carlos <thyago-carlos@hotmail.com>
2021-05-09 01:12:16 -0300

    Cria o controlador de usuários

--- app/Views/topo.php
+++ app/Views/topo.php
@@ -19,7 +19,7 @@
                 </ul>
 
                 <span class="navbar-text">
-                    <a class = "btn btn-info" href="#" data-tooltip="tooltip" title="Não tem uma conta? Cadastre-se">Cadastre-se</a>
+                    <a class = "btn btn-info" href="<?=URL?>/usuarios/cadastrar" data-tooltip="tooltip" title="Não tem uma conta? Cadastre-se">Cadastre-se</a>
                     <a class = "btn btn-info" href="#" data-tooltip="tooltip" title="Tem uma conta? Faça login">Entrar</a>
                 </span>
             </div>
