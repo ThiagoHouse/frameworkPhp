0ed00deccf877bdfdbaed2a7da61512e884c95e1
thyago-carlos <thyago-carlos@hotmail.com>
2021-05-19 01:21:17 -0300

    Ajusta o Layout da navbar

--- app/Views/topo.php
+++ app/Views/topo.php
@@ -1,6 +1,6 @@
 <header class="bg-dark">
     <div class="container">
-        <nav class="navbar navbar-expand-sm navbar-dark">
+        <nav class="navbar navbar-expand-sm navbar-dark ">
             <a class="navbar-brand" href="<?=URL?>">FrameworPHP</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse"
             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -18,11 +18,12 @@
                     </li>
                 </ul>
 
-                <span class="navbar-text">
+                
+            </div>
+            <span class="float-right navbar-text">
                     <a class = "btn btn-info" href="<?=URL?>/usuarios/cadastrar" data-tooltip="tooltip" title="Não tem uma conta? Cadastre-se">Cadastre-se</a>
                     <a class = "btn btn-info" href="#" data-tooltip="tooltip" title="Tem uma conta? Faça login">Entrar</a>
-                </span>
-            </div>
+            </span>
         </nav>    
     </div>
 </header>
