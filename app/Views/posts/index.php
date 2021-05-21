5b5fa876a561d5a74a3329906d356ea8cb5c31ff
thyago-carlos <thyago-carlos@hotmail.com>
2021-05-21 10:10:20 -0300

    Corrige alinhamento do botão

--- app/Views/posts/index.php
+++ app/Views/posts/index.php
@@ -3,12 +3,12 @@
     <div class="card">
         <div class="card-header bg-info text-white">
             POSTAGENS
-            <div class="float-right">
-                <a href="<?=URL?>/posts/cadastrar" class="btn btn-light">Escrever</a>
-            </div>
+                <div class="float-end">
+                    <a href="<?=URL?>/posts/cadastrar" class="btn btn-light">Escrever</a>
+                </div>
         </div>
         <div class="card-body">
-            <p>Listar posts aqui </p>
+            <p>Listar posts aqui</p>
         </div>
     </div>
 
