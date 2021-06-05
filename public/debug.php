9efd2dfa1b0838684de86d9d471bb8e452239689
thyago-carlos <thyago-carlos@hotmail.com>
2021-06-05 00:53:39 -0300

    Valida arquivos antes do upload

--- public/debug.php
+++ public/debug.php
@@ -1,28 +1,47 @@
 <?php
-    var_dump($_FILES);
-    echo '<hr>';
+var_dump($_FILES);
+echo '<hr>';
 
-    if(isset($_FILES['arquivo'])):
-        $diretorio = '../public/uploads/';
-        $arquivo = $_FILES['arquivo']['name'];
+if (isset($_FILES['arquivo'])) :
+    $diretorio = '../public/uploads/';
+    $arquivo = $_FILES['arquivo']['name'];
+    $tipo = $_FILES['arquivo']['type'];
+    $tamanhoArquivo = $_FILES['arquivo']['size'];
 
-        if (!file_exists($diretorio) && !is_dir($diretorio)):
+    $extensoesValida = ['png', 'jpg'];
+    $tiposValidos = ['image/jpeg', 'image/png'];
+    $tamanhoValido = ['image/jpeg', 'image/png'];
+
+    $extensao = pathinfo($arquivo, PATHINFO_EXTENSION);
+    var_dump($extensao);
+
+    if (!in_array($extensao, $extensoesValida)) :
+        echo "A extensão não é permitida";
+    elseif (!in_array($tipo, $tiposValidos)) :
+        echo 'Tipo invalido';
+    elseif ($tamanhoArquivo > 3 * (1024 * 1024)) :
+        echo 'Arquivo muito grande';
+    else :
+        if (!file_exists($diretorio) && !is_dir($diretorio)) :
             mkdir($diretorio, 0777);
         endif;
 
-        if (file_exists($diretorio.DIRECTORY_SEPARATOR.$arquivo)):
+        if (file_exists($diretorio . DIRECTORY_SEPARATOR . $arquivo)) :
             $nome = pathinfo($arquivo, PATHINFO_FILENAME);
-            $arquivo = $nome.'-'.uniqid().strrchr($arquivo, '.');
+            $arquivo = $nome . '-' . uniqid() . strrchr($arquivo, '.');
         endif;
 
-        if(move_uploaded_file($_FILES['arquivo']['tmp_name'], $diretorio.$arquivo)):
+        if (move_uploaded_file($_FILES['arquivo']['tmp_name'], $diretorio . $arquivo)) :
             echo 'Upload realizado com sucesso';
-        else:
+        else :
             echo 'Erro ao fazer upload';
-        endif;        
+        endif;
     endif;
 
-    echo '<hr>';
+
+endif;
+
+echo '<hr>';
 
 
 ?>
@@ -32,4 +51,3 @@
     <input type="file" name="arquivo">
     <input type="submit" value="enviar">
 </form>
-
